ed0fdc392783104500ac79b650081d975363b429
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-04-05 16:55:02 -0400

    Add files via upload

--- exam-mastery/src/App.js
+++ exam-mastery/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./Pages/global/Topbar";
+import Sidebar from "./Pages/global/Sidebar";
+import Dashboard from "./Pages/dashboard";
+import Team from "./Pages/Team";
+import Login from "./Pages/login";
+import Contacts from "./Pages/contacts";
+import FAQ from "./Pages/faq";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              {<Route path="/" element={<Dashboard />}/>}
+              {<Route path="/team" element={<Team />} />}
+              {<Route path="/contacts" element={<Contacts />} />}
+              {<Route path="/faq" element={<FAQ />} />}
+              {<Route path="/login" element={<Login />} />}
+              {/* <Route path="/" element={<Dashboard />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+  
 }
 
+  
 export default App;
---
+++ exam-mastery/src/data/mockData.js
@@ -0,0 +1,70 @@
+
+
+export const mockDataTeam = [
+  {
+    id: 1,
+    name: "Jinal Mamaniya",
+    email: "jinalmamaniya@gmail.com",
+    age: 35,
+    phone: "(665)121-5454",
+    access: "Co-Founder",
+  },
+  {
+    id: 2,
+    name: "Shreya Ghate",
+    email: "shreyaghate@gmail.com",
+    age: 42,
+    phone: "(421)314-2288",
+    access: "Co-Founder",
+  },
+  {
+    id: 3,
+    name: "Shweta Bulchandani",
+    email: "shwetabulchandani@gmail.com",
+    age: 45,
+    phone: "(422)982-6739",
+    access: "Teacher",
+  },
+  {
+    id: 4,
+    name: "Rohit Panicker",
+    email: "rohitpanicker@gmail.com",
+    age: 16,
+    phone: "(921)425-6742",
+    access: "Teacher",
+  },
+  {
+    id: 5,
+    name: "Jatinder Bali",
+    email: "jatinderbali@gmail.com",
+    age: 31,
+    phone: "(421)445-1189",
+    access: "Teacher",
+  },
+  {
+    id: 6,
+    name: "Mayur Khandetod",
+    email: "mayurkhadetod@gmail.com",
+    age: 50,
+    phone: "(232)545-6483",
+    access: "Teacher",
+  },
+  {
+    id: 7,
+    name: "Salman Khan",
+    email: "salmankhan@gmail.com",
+    age: 44,
+    phone: "(543)124-0123",
+    access: "Material Creator",
+  },
+  {
+    id: 8,
+    name: "Mark Jacobs",
+    email: "markjacobs@gmail.com",
+    age: 35,
+    phone: "(543)124-0123",
+    access: "Material Creator",
+  },
+];
+
+
