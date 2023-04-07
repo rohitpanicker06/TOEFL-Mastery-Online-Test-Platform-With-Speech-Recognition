21f9128a853fe4bb422431a3eb963ff0fc8014fd
Rutuja Mandlik <mandlik.r@northeastern.edu>
2023-04-07 01:59:23 -0400

    Added Leaderboard & Study Material components on Student Dashboard

--- exam-mastery/src/Pages/Student/StudentDashboard.jsx
+++ exam-mastery/src/Pages/Student/StudentDashboard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { Box, Typography, Container, Grid } from "@mui/material";
+import { Box, Typography, Container, Grid, Link } from "@mui/material";
 import Summary from "../../Components/Student/Summary";
 import { useEffect, useState } from "react";
 import SectionWiseComparisonChart from "./SectionWiseComparisonChart";
+import SomeComponent from "../../Components/Student/SomeComponent";
+import Leaderboard from "../../Components/Student/Leaderboard";
+import Iconify from "../../Components/Iconify/Iconify";
+import SuggestedStudyMaterial from "../../Components/Student/SuggestedStudyMaterial";
+import Avatar from "@mui/material/Avatar";
 
 const StudentDashboard = () => {
   const [userData, setUserData] = useState({});
@@ -112,6 +117,106 @@ const StudentDashboard = () => {
               {
                 name: "Speaking",
                 data: [5, 8, 4, 7, 6],
+              },
+            ]}
+          />
+        </Grid>
+
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
+          data-aos="fade-up"
+          data-aos-delay="900"
+        >
+          <SomeComponent
+            title="Today's Lesson"
+            list={timeline.map((item, index) => ({
+              id: index,
+              title: item.title,
+              type: `order${index + 1}`,
+              category: item.type,
+              time: new Date(item.date).toLocaleDateString(),
+            }))}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={8} data-aos="fade-in">
+          <Leaderboard
+            title="Leaderboard"
+            subheader="Top 5 students on our platform"
+            list={[...Array(5)].map((_, index) => ({
+              id: index,
+              title: [
+                "Raj Shekhawat",
+                "John Miller",
+                "Melanie Diaz",
+                "Cece Patel",
+                "Xinzhuo Liu",
+              ][index],
+              description: [
+                "General",
+                "Academic",
+                "General",
+                "Academic",
+                "Academic",
+              ][index],
+              image: `https://www.google.com/url?sa=i&url=https%3A%2F%2Fuxwing.com%2Fuser-profile-icon%2F&psig=AOvVaw0KejkpKrPBU69l9kI0VFnh&ust=1680933016016000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKDLj9GJl_4CFQAAAAAdAAAAABAQ`,
+
+              proficiency: 8.5 - index,
+            }))}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={4} data-aos="fade-in">
+          <SuggestedStudyMaterial
+            title="Study Material"
+            subheader="Handpicked recommendations for you"
+            list={[
+              {
+                name: "Reading",
+                value: "Youtube",
+                icon: (
+                  <Iconify icon={"uit:youtube"} color="#1877F2" width={32} />
+                ),
+                link: "https://www.youtube.com/",
+              },
+              {
+                name: "Listening",
+                value: "BYJU",
+                icon: (
+                  <Iconify
+                    icon={"simple-icons:byjus"}
+                    color="#DF3E30"
+                    width={32}
+                  />
+                ),
+                link: "https://www.byju.com/",
+              },
+              {
+                name: "Speaking",
+                value: "Udemy",
+                icon: (
+                  <Iconify
+                    icon={"logos:udemy-icon"}
+                    color="#006097"
+                    width={32}
+                  />
+                ),
+                link: "https://www.udemy.com/",
+              },
+              {
+                name: "Writing",
+                value: "Coursera",
+                icon: (
+                  <Iconify
+                    icon={"academicons:coursera-square"}
+                    color="#1C9CEA"
+                    width={32}
+                  />
+                ),
+                link: "https://www.coursera.com/",
               },
             ]}
           />
