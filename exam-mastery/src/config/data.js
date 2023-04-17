0d59c0c0ff052a51b699c16e752a6daca451da91
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-04-17 13:57:26 -0400

    Adding Blog Section

---
+++ exam-mastery/src/config/data.js
@@ -0,0 +1,75 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'What is a Good TOEFL Score? ',
+    category: 'score',
+    subCategory: ['toefl', 'usa', 'college'],
+    description:
+      "Unlike other tests, you can’t really “pass” or “fail” the TOEFL. So what’s a good TOEFL score? Our experts break it down for you.",
+    authorName: 'DAVID RECINE',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2022',
+    cover: '/assets/images/toefl.png',
+  },
+  {
+    id: 2,
+    title: 'One Month TOEFL Study Plan | How to Prepare for the TOEFL',
+    category: 'plan',
+    subCategory: ['study', 'prepare', 'toefl'],
+    description:
+      "This month-long TOEFL study plan is mostly designed to get you familiar with the TOEFL format and question types, but with the ultimate aim of getting your TOEFL scores to an acceptable level. If you’ve followed this one month TOEFL study schedule, you should be in good shape for the test!",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/studyplan.jpeg',
+  },
+  {
+    id: 3,
+    title: 'The Ultimate TOEFL Vocabulary List',
+    category: 'toef',
+    subCategory: ['vocabulary', 'toefl', 'list'],
+    description:
+      "TOEFL (Test of English as a Foreign Language) is a standardized test to measure the English language ability of non-native speakers wishing to enrol in English-speaking universities. It is an English proficiency assessment for international students and institutions. Just like IELTS and Duolingo it tests all four language skills Reading, Listening, Speaking and Writing. Polished vocabulary is an important part of the TOEFL Syllabus as it helps students in each exam segment. This blog focuses on important words, exercises and tips to improve vocabulary for TOEFL and will also provide an Ultimate TOEFL vocabulary list.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Ultimate-Toefl-Vocabulary.png',
+  },
+  {
+    id: 4,
+    title: 'Universities Accepting TOEFL Score 75 and Below!',
+    category: 'toefl',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "Many schools and institutions have set standards for how high SAT and ACT scores should be. What about the TOEFL, though? Do universities have TOEFL requirements as well? Yes, many international colleges require TOEFL scores as part of the application process. However, the exact TOEFL score you’ll need for admission is determined by the institution to which you’re applying. Are you worried about which school accepts applicants with low scores in the TOEFL Exam? Then, don’t worry, we’ve got you covered. In this blog, we will see some International universities accepting TOEFL score 75 or below.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Universities-Accepting-TOEFL-Score-of-75.png',
+  },
+  {
+    id: 5,
+    title: 'TOEFL Sample Speaking Questions',
+    category: 'toefl',
+    subCategory: ['speaking', 'questions', 'toefl'],
+    description:
+      "An important part and parcel of your study abroad journey is qualifying for the English proficiency tests. One renowned name in the list of language proficiency tests is TOEFL. Indian outbound students take this exam to apply at the universities of English-speaking countries. Conducted by the English Testing System, TOEFL focuses on testing the fluency of the applicants on 4 main learning parameters- Reading, Writing, Speaking, and Listening. If you aspire to outshine your TOEFL score, here is a blog to help you with some TOEFL sample speaking questions.",
+    authorName: 'Ayushi Sharma',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/TOEFL-Sample-Speaking-Questions.png',
+  },
+  {
+    id: 6,
+    title: 'Test of English as a Foreign Language (TOEFL)',
+    category: 'toefl',
+    subCategory: ['exam', 'information', 'test'],
+    description:
+      "If you are planning to study, work, or settle in an English-speaking country, you are required to qualify for an English test to prove your efficiency in the language. Even though there are a number of proficiency tests like IELTS and PTE, the type of exam you take depends on the country you wish to kickstart your career. Apart from the aforementioned exams, the Test of English as a Foreign Language (TOEFL) is another popular language test which is conducted by the ETS. You can take TOEFL in iBT or PBT mode where the former is  Internet-based while the latter is conducted in offline mode.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover:
+      '/assets/images/Test-of-English-as-a-Foreign-Language.png',
+  },
+];
