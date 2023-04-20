b22a14803d0100047d8f994dfd1d74383da82f1f
JatinderBali17 <bali.j@northeastern.edu>
2023-04-20 00:39:25 -0400

    Logout Button and Role dropdown for display

---
+++ exam-mastery/src/Components/LoginForms/DropdownProfile.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const DropDownProfile=() => {
+    return (
+        <div className='flex flex-col dropDownProfile'>
+            <ul className='flex flex-col gap-4'>
+                <p></p>
+                <Button>Logout</Button>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default DropDownProfile
